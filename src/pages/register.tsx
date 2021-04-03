a4941b68c774c0a772d7f1ea3412ce8f59f8420e
krishnan-tech <krishnannavadia@gmail.com>
2021-04-03 22:07:00 +0530

    Added Components, pages, utils and codegen

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,9 +1,47 @@
 import React from 'react'
-
+import { Form, Formik } from 'formik';
+import { Box, Button } from '@chakra-ui/react';
+import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router'
 interface registerProps {
 
 }
 
-export const register: React.FC<registerProps> = ({ }) => {
-    return ();
+
+const Register: React.FC<registerProps> = ({ }) => {
+    const router = useRouter();
+    const [, register] = useRegisterMutation();
+    return (
+        <Wrapper variant="small">
+            <Formik
+                initialValues={{ username: '', password: "" }}
+                onSubmit={async (values, { setErrors }) => {
+                    const response = await register(values);
+                    if (response.data?.register.errors) {
+                        setErrors(toErrorMap(response.data.register.errors));
+                    } else if (response.data?.register.errors) {
+                        // worked
+                        router.push('/');
+                    }
+                }}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <InputField name="username" placeholder="username" label="Username"></InputField>
+                        <Box mt={4}>
+                            <InputField name="password" placeholder="password" label="Password" type="password"></InputField>
+                        </Box>
+                        <Button mt={4} type="submit" isLoading={isSubmitting} colorScheme='teal'>
+                            Register
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
+    )
 }
+
+export default Register;
